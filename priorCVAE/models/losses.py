fa9d92e34afc042a42a1ae3ccdd9509709cc2ec3
Elizaveta Semenova <elizaveta.p.semenova@gmail.com>
2023-06-19 15:19:52 +0100

    simplify trainig loop

--- priorCVAE/models/losses.py
+++ priorCVAE/models/losses.py
@@ -11,18 +11,3 @@ def kl_divergence(mean, logvar):
 def rcl(y, reconstructed_y):
     """ sum or mean? """
     return jnp.sum(optax.l2_loss(reconstructed_y, y)) 
-
-def vae_loss(mean, logvar, y, reconstructed_y):
-    rcl_loss = rcl(reconstructed_y, y).mean()
-    kld_loss = kl_divergence(mean, logvar).mean()
-    return rcl_loss + kld_loss
-   
-
-def compute_metrics(recon_y, y, mean, logvar):
-  rcl_loss = rcl(recon_y, y).mean()
-  kld_loss = kl_divergence(mean, logvar).mean()
-  return {
-      'rcl':  rcl_loss,
-      'kld':  kld_loss,
-      'loss': rcl_loss + kld_loss
-  }
