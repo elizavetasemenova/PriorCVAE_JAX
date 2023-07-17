c074eb777e6023a46832660a168e98a978539d78
EricBrine <eric.brine22@imperial.ac.uk>
2023-07-17 19:16:16 +0100

    wandb for experiment tracking

--- model_validation/run.py
+++ model_validation/run.py
@@ -4,7 +4,7 @@ from jax import random
 import scipy.stats as stats
 from flax.training import train_state
 import optax
-import mlflow
+import wandb
 
 from priorCVAE.models import VAE, MLPEncoder, MLPDecoder
 from priorCVAE.diagnostics import (
@@ -21,96 +21,108 @@ from model_validation.utils import generate_vae_samples
 from model_validation.model_validation_tests import mean_bootstrap_interval_contains_zero
 
 
-# Kernel parameters
-kernel_name = "SquaredExponentialKernel"
-lengthscale = 0.2
-kernel_variance = 1.0
+config = {
+    "kernel": {
+        "name": "SquaredExponentialKernel",
+        "lengthscale": 0.2,
+        "variance": 1.0
+    },
+    "grid": {
+        "size": 50,
+        "x_min": 0,
+        "x_max": 1
+    },
+    "model": {
+        "input_dim": 50,
+        "hidden_dim": 40,
+        "latent_dim": 30,
+        "output_dim": 50,
+        "vae_variance": 1.0
+    },
+    "training": {
+        "num_data": 4000,
+        "batch_size": 100,
+        "num_epochs": 20
+    },
+    "test": {
+        "num_samples": 10000
+    }
+}
 
-grid_size = 50
-grid = jnp.linspace(0, 1, grid_size)
+kernel_name = config["kernel"]["name"]
+lengthscale = config["kernel"]["lengthscale"]
+kernel_variance = config["kernel"]["variance"]
 
-# Model parameters
-input_dim = grid_size
-hidden_dim = 40
-latent_dim = 30
-output_dim = input_dim
-vae_variance = 1.0
+grid_size = config["grid"]["size"]
+x_min = config["grid"]["x_min"]
+x_max = config["grid"]["x_max"]
+grid = jnp.linspace(x_min, x_max, grid_size)
+
+input_dim = config["model"]["input_dim"]
+hidden_dim = config["model"]["hidden_dim"]
+latent_dim = config["model"]["latent_dim"]
+output_dim = config["model"]["output_dim"]
+vae_variance = config["model"]["vae_variance"]
 
 # Training parameters
-num_data = 4000
-batch_size = 100
-num_epochs = 200
+num_data = config["training"]["num_data"]
+batch_size = config["training"]["batch_size"]
+num_epochs = config["training"]["num_epochs"]
 
 # Test parameters
-num_samples = 1000
+num_samples = config["test"]["num_samples"]
 
-def run():
 
-    mlflow.start_run(run_name="SquaredExponentialKernel")
-    mlflow.log_param("grid_size", grid_size)
+def main():
+
+
+    run = wandb.init(project="first-sweeps", config=config)
     kernel = SquaredExponential(lengthscale, kernel_variance)(grid, grid)
 
     train_set = GPDataset(
-        n_data=input_dim, n_samples=num_data, lengthscale=lengthscale
+        n_data=input_dim, n_samples=num_data, lengthscale=lengthscale, x_lim_low=x_min, x_lim_high=x_max
     )
-    test_set = GPDataset(
-        n_data=input_dim, n_samples=num_data, lengthscale=lengthscale
+    val_set = GPDataset(
+        n_data=input_dim, n_samples=num_data, lengthscale=lengthscale, x_lim_low=x_min, x_lim_high=x_max
     )
     train_loader, = create_data_loaders(
         train_set, batch_size=batch_size
     )
-    test_loader, = create_data_loaders(
-        test_set, batch_size=batch_size
+    val_loader, = create_data_loaders(
+        val_set, batch_size=batch_size
     )
-
-    log_params()
 
     key = jax.random.PRNGKey(0) 
     key, train_key, test_key = random.split(key, 3)
 
-    state = train_vae(train_key, train_loader, test_loader)
+    state = train_vae(train_key, train_loader, val_loader)
     decoder_params = state.params['decoder']
 
     run_tests(key, decoder_params, kernel)    
-    
+    wandb.finish()
 
 def run_tests(key, decoder_params, kernel):
     decoder = MLPDecoder(hidden_dim, output_dim)
     res = mean_bootstrap_interval_contains_zero(key, decoder_params, decoder, latent_dim)
-    mlflow.log_metric("mean_bootstrap_interval_contains_zero", res)
+    wandb.log({"mean_bootstrap_interval_contains_zero": res})
 
     samples = generate_vae_samples(key, decoder_params, decoder, num_samples, latent_dim)
     norm = frobenius_norm_of_kernel_diff(samples, kernel)
-    mlflow.log_metric("norm_of_kernel_diff", norm)
+    wandb.log({"norm_of_kernel_diff": norm})
 
-    fig, ax = plot_realizations(grid, samples, "VAE samples")
-    mlflow.log_figure(fig, 'vae_realizations.png')
+    # fig, ax = plot_realizations(grid, samples, "VAE samples")
+    # wandb.log({"vae_realizations": wandb.Image(fig)})
 
-    cov_matrix = compute_empirical_covariance(samples)
-    fig, ax = plot_covariance_matrix(cov_matrix, "Empirical covariance")
-    mlflow.log_figure(fig, 'vae_covariance.png')
+    # cov_matrix = compute_empirical_covariance(samples)
+    # fig, ax = plot_covariance_matrix(cov_matrix, "Empirical covariance")
+    # wandb.log({"empirical_covariance": wandb.Image(fig)})
 
-    fig, ax = plot_covariance_matrix(kernel, kernel_name)
-    mlflow.log_figure(fig, 'kernel.png')
+    # fig, ax = plot_covariance_matrix(kernel, kernel_name)
+    # wandb.log({"kernel": wandb.Image(fig)})
 
-
-def log_params():
-    mlflow.log_param("kernel_name", kernel_name)
-    mlflow.log_param("grid_size", grid_size)
-    mlflow.log_param("lengthscale", lengthscale)
-    mlflow.log_param("kernel_variance", kernel_variance)
-    mlflow.log_param("input_dim", input_dim)
-    mlflow.log_param("hidden_dim", hidden_dim)
-    mlflow.log_param("latent_dim", latent_dim)
-    mlflow.log_param("output_dim", output_dim)
-    mlflow.log_param("vae_variance", vae_variance)
-    mlflow.log_param("training_set_size", num_data)
-    mlflow.log_param("batch_size", batch_size)
-    mlflow.log_param("num_epochs", num_epochs)
-    mlflow.log_param("num_samples", num_samples)
 
 # TODO: use training methods in module
-def train_vae(key, train_loader, test_loader):
+def train_vae(key, train_loader, val_loader):
     encoder = MLPEncoder(hidden_dim, latent_dim)
     decoder = MLPDecoder(hidden_dim, output_dim)
     model = VAE(encoder, decoder)
@@ -120,7 +132,7 @@ def train_vae(key, train_loader, test_loader):
         def loss_fn(params, z_rng):
             _, y, _ = batch 
             y_hat, z_mu, z_logvar = model.apply({'params': params}, y, z_rng) 
-            rcl_loss = scaled_sum_squared_loss(y_hat, y, vae_variance)
+            rcl_loss = scaled_sum_squared_loss(y_hat, y, wandb.config.model['vae_variance'])
             kld_loss = kl_divergence(z_mu, z_logvar)
             loss = rcl_loss + kld_loss
             return loss
@@ -133,7 +145,7 @@ def train_vae(key, train_loader, test_loader):
         def loss_fn(params, z_rng):
             _, y, _ = batch 
             y_hat, z_mu, z_logvar = model.apply({'params': params}, y, z_rng) 
-            rcl_loss = scaled_sum_squared_loss(y_hat, y, vae_variance)
+            rcl_loss = scaled_sum_squared_loss(y_hat, y, wandb.config.model['vae_variance'])
             kld_loss = kl_divergence(z_mu, z_logvar)
             loss = rcl_loss + kld_loss
             return loss
@@ -154,24 +166,48 @@ def train_vae(key, train_loader, test_loader):
     state = train_state.TrainState.create(apply_fn=model, params=params, tx=optimizer)
 
     _loss_train = []
-    _loss_test = []
+    _loss_val = []
 
     for epoch in range(num_epochs):
-        for _, batch_train in enumerate(train_loader):
+        for _, batch in enumerate(train_loader):
             z_key, key = random.split(z_key)
-            state, loss_train = train_step(state, batch_train, key)
+            state, loss_train = train_step(state, batch, key)
             _loss_train.append(loss_train)
 
-    for _, batch_test in enumerate(test_loader):
+        for _, batch in enumerate(val_loader):
 
-        eval_key, key = random.split(eval_key)
-        loss_test = eval(state, batch_test, key)
-        _loss_test.append(loss_test)
-    mlflow.log_metric("train_loss", loss_train)
-    mlflow.log_metric("test_loss", loss_test)
+            eval_key, key = random.split(eval_key)
+            loss_test = eval(state, batch, key)
+            _loss_val.append(loss_test)
+
+        wandb.log({"train_loss": loss_train, "val_loss": loss_test})
 
     return state
 
 
 if __name__ == "__main__":
-    run()
+    # main()
+
+    sweep_configuration = {
+        'method': 'bayes',
+        'metric': 
+        {
+            'goal': 'minimize', 
+            'name': 'norm_of_kernel_diff'
+            },
+        'parameters': 
+        {
+            'model': {
+                'parameters': { 
+                    'vae_variance': {'max': 3., 'min': 1.}
+                }
+            }
+        }
+    }
+
+    sweep_id = wandb.sweep(
+        sweep=sweep_configuration, 
+        project='my-first-sweep'
+        )
+
+    wandb.agent(sweep_id, function=main, count=8)
