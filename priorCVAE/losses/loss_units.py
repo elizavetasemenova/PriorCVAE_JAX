loss units fixed for mean over batch

priorCVAE/losses/loss_units.py
@@ -19,12 +19,19 @@ def kl_divergence(mean: jnp.ndarray, logvar: jnp.ndarray) -> jnp.ndarray:
 
     Detailed derivation can be found here: https://learnopencv.com/variational-autoencoder-in-tensorflow/
 
-    :param mean: the mean of the Gaussian distribution with shape (N,).
-    :param logvar: the log-variance of the Gaussian distribution with shape (N,) i.e. only diagonal values considered.
+    :param mean: the mean of the Gaussian distribution with shape (B, D).
+    :param logvar: the log-variance of the Gaussian distribution with shape (B, D) i.e. only diagonal values considered.
 
     :return: the KL divergence value.
+
+    Note: We mean over the batch values.
+
     """
-    return -0.5 * jnp.sum(1 + logvar - jnp.square(mean) - jnp.exp(logvar))
+
+    assert len(mean.shape) == len(logvar.shape) == 2
+    assert mean.shape == logvar.shape
+
+    return jnp.mean(-0.5 * jnp.sum(1 + logvar - jnp.square(mean) - jnp.exp(logvar), axis=-1), axis=0)
 
 
 @jax.jit
@@ -38,14 +45,17 @@ def scaled_sum_squared_loss(y: jnp.ndarray, reconstructed_y: jnp.ndarray, vae_va
 
     -1 * log N (y | y', sigma) \approx -0.5 ((y - y'/sigma)^2)
 
-    :param y: the ground-truth value of y with shape (N, D).
-    :param reconstructed_y: the reconstructed value of y with shape (N, D).
+    :param y: the ground-truth value of y with shape (B, D).
+    :param reconstructed_y: the reconstructed value of y with shape (B, D).
     :param vae_var: a float value representing the varianc of the VAE.
 
     :returns: the loss value
+
+    Note: We mean over the batch values.
     """
+    assert len(y.shape) == len(reconstructed_y.shape) == 2
     assert y.shape == reconstructed_y.shape
-    return 0.5 * jnp.sum((reconstructed_y - y) ** 2 / vae_var)
+    return jnp.mean(0.5 * jnp.sum((reconstructed_y - y) ** 2 / vae_var, axis=-1), 0)
 
 
 @jax.jit
@@ -55,13 +65,14 @@ def mean_squared_loss(y: jnp.ndarray, reconstructed_y: jnp.ndarray) -> jnp.ndarr
 
     L(y, y') = mean(((y - y')^2))
 
-    :param y: the ground-truth value of y with shape (N, D).
-    :param reconstructed_y: the reconstructed value of y with shape (N, D).
+    :param y: the ground-truth value of y with shape (B, D).
+    :param reconstructed_y: the reconstructed value of y with shape (B, D).
 
     :returns: the loss value
     """
+    assert len(y.shape) == len(reconstructed_y.shape) == 2
     assert y.shape == reconstructed_y.shape
-    return jnp.mean((reconstructed_y - y) ** 2)
+    return jnp.mean((reconstructed_y - y) ** 2, axis=-1)
 
 
 @partial(jax.jit, static_argnames=['kernel', 'efficient_grads', 'biased'])
@@ -126,6 +137,7 @@ def Gaussian_NLL(y: jnp.ndarray, reconstructed_y_m: jnp.ndarray, reconstructed_y
     Note: We do not calculate the constant term here.
     """
     assert y.shape == reconstructed_y_m.shape == reconstructed_y_logvar.shape
+    assert len(y.shape) == 2
 
     determinant_term = jnp.sum(reconstructed_y_logvar, axis=-1)
     S_inv = 1 / jnp.exp(reconstructed_y_logvar)
@@ -134,4 +146,4 @@ def Gaussian_NLL(y: jnp.ndarray, reconstructed_y_m: jnp.ndarray, reconstructed_y
 
     assert determinant_term.shape == diff_term.shape == (y.shape[0], )
     nll_val = -0.5 * (determinant_term + diff_term)
-    return -1 * jnp.sum(nll_val)
+    return -1 * jnp.mean(nll_val)