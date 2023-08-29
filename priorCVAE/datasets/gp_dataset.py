a1c81f5289ed2d8107508d839ca538af409fe295
prakhar.verma <prakhar.verma7@gmail.com>
2023-08-29 20:52:29 +0100

    discrete ls option added

--- priorCVAE/datasets/gp_dataset.py
+++ priorCVAE/datasets/gp_dataset.py
@@ -2,12 +2,13 @@
 Gaussian process dataset.
 
 """
-
+from typing import Union, List
 import random as rnd
+
 import jax.numpy as jnp
-import jax.random
 from jax import random
 from numpyro.infer import Predictive
+from omegaconf import ListConfig
 
 from priorCVAE.priors import GP, Kernel
 
@@ -18,17 +19,22 @@ class GPDataset:
 
     """
 
-    def __init__(self, kernel: Kernel, x: jnp.ndarray, sample_lengthscale: bool = False):
+    def __init__(self, kernel: Kernel, x: jnp.ndarray, sample_lengthscale: bool = False,
+                 lengthscale_options: Union[List, jnp.ndarray] = None):
         """
         Initialize the Gaussian Process dataset class.
 
         :param kernel: Kernel to be used.
         :param sample_lengthscale: whether to sample lengthscale for the kernel or not. Defaults to False.
         :param x: jax.numpy.ndarray of the x grid used to generate sample from the GP.
+        :param lengthscale_options: a list or jnp.ndarray of lengthscale options to choose from.
         """
         self.sample_lengthscale = sample_lengthscale
         self.kernel = kernel
         self.x = x
+        if isinstance(lengthscale_options, ListConfig):
+            lengthscale_options = jnp.array(lengthscale_options)
+        self.lengthscale_options = lengthscale_options
 
     def simulatedata(self, n_samples: int = 10000) -> [jnp.ndarray, jnp.ndarray, jnp.ndarray]:
         """
@@ -45,10 +51,10 @@ class GPDataset:
 
         gp_predictive = Predictive(GP, num_samples=n_samples)
         all_draws = gp_predictive(rng_key, x=self.x, kernel=self.kernel, jitter=1e-5,
-                                  sample_lengthscale=self.sample_lengthscale)
+                                  sample_lengthscale=self.sample_lengthscale,
+                                  lengthscale_options=self.lengthscale_options)
 
         ls_draws = jnp.array(all_draws['ls'])
         gp_draws = jnp.array(all_draws['y'])
 
         return self.x[None, ...].repeat(n_samples, axis=0), gp_draws, ls_draws
-
