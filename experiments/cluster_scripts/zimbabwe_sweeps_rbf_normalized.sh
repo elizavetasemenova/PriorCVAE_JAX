5cb43e3de55cb316c210149ffe19cf7e3be212d2
Elizaveta Semenova <elizaveta.p.semenova@gmail.com>
2024-02-09 10:20:20 +0000

    zimbabwe cluster scripts

---
+++ experiments/cluster_scripts/zimbabwe_sweeps_rbf_normalized.sh
@@ -0,0 +1,16 @@
+  #!/bin/sh
+  wandbusername=${1:-null}
+
+  for kernel in squared_exponential; do
+      for hidden_dim in 30 50 64; do
+          for latent_dim in 30 50 64; do
+	      for reconstruction_scaling in 0.1 0.5 1 2 5; do
+                  sbatch zimbabwe_sweeps.slurm $wandbusername $reconstruction_scaling $hidden_dim $latent_dim $kernel True
+		  sleep 600
+	      done
+	      sleep 600
+	  done
+	  sleep 600
+      done
+      sleep 600
+  done
